759078cc47c0e2dbf48c61605f328a0dd6f0731b
chefboyrdeuce <jpldeuce@gmail.com>
2020-02-18 20:54:54 -0500

    setting up index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code away!
+const express = require("express")
+const postRouter =require("./posts/postRouter")
+const userRouter = require("./users/userRouter")
+
+const server = express()
+const port = 4000
+
+server.use(express.json())
+server.use("/", postRouter)
+server.use("/api/user", userRouter)
+
+server.listen(port, () => {
+    console.log(`Server running at hhtp://localhost:${port}`)
+})
+
+
